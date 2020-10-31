ea51895f8c41b214c19f6da7d9f1708bbff8da39
Gy <406631892@qq.com>
2020-10-31 22:28:11 +0800

    完成expenseTagData

--- src/lib/expenseTagData.tsx
+++ src/lib/expenseTagData.tsx
@@ -2,5 +2,10 @@ import {tagItemType} from './tagItemType';
 
 
 export const expenseTagData:tagItemType[] = [
-  {id:'others',tagName:'其它'}
+  {id:'others',tagName:'其它'},
+  {id:'salary',tagName:'工资'},
+  {id:'bonus',tagName: '奖金'},
+  {id:'borrow',tagName: '借入'},
+  {id:'interest',tagName: '利息'},
+  {id:'invest',tagName: '投资'}
 ]

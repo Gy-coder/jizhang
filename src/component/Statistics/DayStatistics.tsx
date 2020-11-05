7deae85bea3fbd73038d80dc95353f3912e7994b
Gy <406631892@qq.com>
2020-11-05 12:06:32 +0800

    完成DayStatistics.tsx的CSS

--- src/component/Statistics/DayStatistics.tsx
+++ src/component/Statistics/DayStatistics.tsx
@@ -1,12 +1,18 @@
 import React from "react"
+import styled from 'styled-components';
 
-
+const DayStatisticsWrapper = styled.div`
+  margin: 20px 12px;
+  > span {
+    padding-right: 8px;
+  }
+`
 const DayStatistics:React.FC = ()=>{
   return (
-    <div>
+    <DayStatisticsWrapper>
       <span>今日收入 $0</span>
       <span>今日支出 $0</span>
-    </div>
+    </DayStatisticsWrapper>
   )
 }
 
